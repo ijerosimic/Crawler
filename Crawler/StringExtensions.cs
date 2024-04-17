32aa14fb21dd26066edde0196d983f3aa1ac4d1c
Igor Jerosimic <igor.jerosimic@checkout.com>
2024-04-17 10:17:42 +0100

    Work in progress

--- Crawler/StringExtensions.cs
+++ Crawler/StringExtensions.cs
@@ -2,15 +2,6 @@ namespace Crawler;
 
 public static class StringExtensions
 {
-    public static string GetNormalisedUrl(this string domain, string relativePath)
-    {
-        if (relativePath.StartsWith(domain))
-            return relativePath.NormalizeUrl();
-        if (relativePath.StartsWith('/') && relativePath.Length > 1)
-            return (domain + relativePath).NormalizeUrl();
-        return string.Empty;
-    }
-
     public static string NormalizeUrl(this string url)
     {
         if (string.IsNullOrWhiteSpace(url))
@@ -19,16 +10,6 @@ public static class StringExtensions
         if (!Uri.TryCreate(url, UriKind.RelativeOrAbsolute, out _))
             return string.Empty;
 
-        var result = url.Split('?', '#')[0].TrimEnd('/');
-
-        return string
-            .Join("/", result
-                .Split('/')
-                .Select(part =>
-                    int.TryParse(part, out _)
-                    || Guid.TryParse(part, out _)
-                    || DateTime.TryParse(part, out _)
-                        ? "0"
-                        : part));
+        return url.Split('?', '#')[0].TrimEnd('/');
     }
 }
