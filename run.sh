3f5f68b1735df79b55db919453965846d4d3c93c
Igor Jerosimic <igor.jerosimic@checkout.com>
2024-04-19 12:33:25 +0100

    Work in progress

--- run.sh
+++ run.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-dotnet publish -c Release -o out
-docker build -t crawler-image -f ./Crawler/Dockerfile .
-docker create --name crawler-main crawler-image
 docker run --attach STDOUT crawler-image
